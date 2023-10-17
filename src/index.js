d35e2342a3596eff519bf5e7c233fac6880463f8
YuraBarykin <yBarykin@gmail.com>
2023-10-17 15:26:27 +0400

    Правка app компонента, добавление readme файла

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './components/App'
+import App from './components/app-component/App'
 
 const container = document.getElementById('app')
 const root = createRoot(container)
